3988a33b77cef574cc636f12ceb51f92db48ef74
Виктор Исаев <tropicv@mifors.com>
2020-09-15 10:12:44 +0400

    fix

--- classes/orgspace/gtype.php
+++ classes/orgspace/gtype.php
@@ -2,6 +2,17 @@
 
 /** type of goods */
 
+class loker_db extends basic_db {
+  function load_class(){
+      parent::load_class();
+      $this->tadd("name",new str_type(1,"",512));
+      $this->tadd("tel",new str_type(1,"",512));
+      $this->tadd("mail",new str_type(1,"",512));
+			$this->tadd("coment",new text_type(0,"",get_max_clob_size()));
+  }    
+  
+}
+
 class gtype_db extends basic_db {
   function load_class(){
       parent::load_class();
@@ -119,6 +130,90 @@ class gtype extends basic_manager {
      }
      set_ret_point();
      return $this->exit_code(EXIT_OK);     
+  }
+	
+	
+	function order($state){
+    set_title("?????? ?????? ? ???????");    
+
+    $f=new loker_db();
+    $f->create_default();
+
+    switch ($state){
+
+      case INIT_STATE:
+
+       $gd=new goods($this);
+			 $gd->list_all_predmet();
+
+			 $g=new gtype_db();
+
+			 if(get_param("predmet")!=null){
+				$s=new predmet_db();
+				$s->get_obj(get_param("predmet"));
+				d_put($s);
+
+				$g->list_by_predmet($s->getf("id"));	  
+				d_open_node("list_all_gtype","");
+				while($g->lst()){
+					if($g->is_public()){
+					 $tmp["name"]=$g->getf("name");
+					 $tmp["id"]=$g->getf("id");
+					 d_put_node("list_one",$tmp);
+					}
+				}
+				d_close();  
+
+			 }
+
+			 $g->get_obj(get_obj());
+			 d_put($g);
+
+
+			 if(get_param("predmet")!=null){
+			 set_title("ןנוהלועû - ".$s->getf("short_name")." - ".$g->getf("name"));	
+			 } else{
+			 set_title("ןנוהלועû - ".$g->getf("name"));	
+			 }
+
+				return $this->exit_code(EXIT_OK);
+
+      case SAVE_STATE:
+
+
+       $errs=$f->make_obj($this->env->get_post_data());
+       d_put($f);
+       if($errs) return $this->exit_code(EXIT_ERR,$errs);
+
+       // captcha error
+       if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] === $_POST['keystring']) {
+        ;
+       }else{
+        go_to("loker-add");
+        return $this->exit_code(EXIT_EXTERNAL_REDIRECT);
+       }
+
+       $res="<html><body><h3>?????? ?????? ? ???????</h3><br>";
+       $res.="<b>* ???</b>: " .$f->getf("name")."<br>";
+       $res.="<b>???????</b>: " .$f->getf("tel")."<br>";
+       $res.="<b>* E-mail</b>: " .$f->getf("mail")."<br>";
+       $res.="<b>???????????</b>: " .$f->getf("coment")."<br>";
+       $res.="<br><hr><br>Orgspace.ru</body></html>";
+
+       form_to_db("????? \"?????? ?????? ? ???????\"",$res);
+
+       mail_msg_html(get_proper("loker_mail_to"),"????? ?????? ? ????? \"?????? ?????? ? ???????\" ? www.orgspace.ru",$res);
+
+       go_to("loker-add?state=2"); 
+       return $this->exit_code(EXIT_EXTERNAL_REDIRECT);
+
+      case STEP2:
+
+       return $this->exit_code(EXIT_OK);
+
+   }
+   return $this->man->exit_code(NOT_SUPPORT);
+
   }
 
  
