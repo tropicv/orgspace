b2a9e1ce09a34235a23fec95f597818cdf9bd37a
Виктор Исаев <tropicv@mifors.com>
2020-09-15 11:26:09 +0400

    fix

--- classes/orgspace/gtype.php
+++ classes/orgspace/gtype.php
@@ -183,16 +183,16 @@ class gtype extends basic_manager {
        d_put($f);
        if($errs) return $this->exit_code(EXIT_ERR,$errs);
 
-       $res="<html><body><h3>?????? ?????? ? ???????</h3><br>";
-       $res.="<b>* ???</b>: " .$f->getf("name")."<br>";
-       $res.="<b>???????</b>: " .$f->getf("tel")."<br>";
+       $res="<html><body><h3>Узнать больше о локерах</h3><br>";
+       $res.="<b>* Имя</b>: " .$f->getf("name")."<br>";
+       $res.="<b>Телефон</b>: " .$f->getf("tel")."<br>";
        $res.="<b>* E-mail</b>: " .$f->getf("mail")."<br>";
-       $res.="<b>???????????</b>: " .$f->getf("coment")."<br>";
+       $res.="<b>Комментарий</b>: " .$f->getf("coment")."<br>";
        $res.="<br><hr><br>Orgspace.ru</body></html>";
 
-       form_to_db("?????? ?????? ? ???????",$res);
+       form_to_db("Узнать больше о локерах",$res);
 
-       mail_msg_html(get_proper("loker_mail_to"),"????? ?????? ? ????? \"?????? ?????? ? ???????\" ? www.orgspace.ru",$res);
+       mail_msg_html(get_proper("loker_mail_to"),"Новая заявка с формы \"Узнать больше о локерах\" с www.orgspace.ru",$res);
 
        go_to("gtype-order?state=2");  
        return $this->exit_code(EXIT_EXTERNAL_REDIRECT);
