27353e2030b0d7d8ea2dc7190d610f0d2795028e
Виктор Исаев <tropicv@mifors.com>
2020-09-15 10:57:24 +0400

    fix

--- classes/orgspace/gtype.php
+++ classes/orgspace/gtype.php
@@ -144,35 +144,35 @@ class gtype extends basic_manager {
       case INIT_STATE:
 				$errs=$f->make_obj($this->env->get_post_data());
 				d_put($f);
-				// $g=new gtype_db();
+				$g=new gtype_db();
 
-				// if(get_param("predmet")!=null){
-					// $s=new predmet_db();
-					// $s->get_obj(get_param("predmet"));
-					// d_put($s);
+				if(get_param("predmet")!=null){
+					$s=new predmet_db();
+					$s->get_obj(get_param("predmet"));
+					d_put($s);
 
-					// $g->list_by_predmet($s->getf("id"));	  
-					// d_open_node("list_all_gtype","");
-					// while($g->lst()){
-						// if($g->is_public()){
-						 // $tmp["name"]=$g->getf("name");
-						 // $tmp["id"]=$g->getf("id");
-						 // d_put_node("list_one",$tmp);
-						// }
-					// }
-					// d_close();  
+					$g->list_by_predmet($s->getf("id"));	  
+					d_open_node("list_all_gtype","");
+					while($g->lst()){
+						if($g->is_public()){
+						 $tmp["name"]=$g->getf("name");
+						 $tmp["id"]=$g->getf("id");
+						 d_put_node("list_one",$tmp);
+						}
+					}
+					d_close();  
 
-			 // }
+			 }
 
-			 // $g->get_obj(get_obj());
-			 // d_put($g);
+			 $g->get_obj(get_obj());
+			 d_put($g);
 
 
-			 // if(get_param("predmet")!=null){
-				// set_title("ןנוהלועû - ".$s->getf("short_name")." - ".$g->getf("name"));	
-			 // } else{
-				// set_title("ןנוהלועû - ".$g->getf("name"));	
-			 // }
+			 if(get_param("predmet")!=null){
+				set_title("ןנוהלועû - ".$s->getf("short_name")." - ".$g->getf("name"));	
+			 } else{
+				set_title("ןנוהלועû - ".$g->getf("name"));	
+			 }
 
 			return $this->exit_code(EXIT_OK);
 
